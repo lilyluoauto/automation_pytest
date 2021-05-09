d3cb1b15337a0ba24f2803b6a3558cfbb366431c
lilyauto <licy_luo@163.com>
2021-05-09 12:36:18 +0800

    step2&step3

--- util/get_method_name.py
+++ util/get_method_name.py
@@ -26,7 +26,8 @@ class Yoyo():
     def yoyo_1(self):
         print("函数名称：%s" % get__function_name())
         print("name : %s" % sys._getframe().f_code.co_name)
-
+        print("step2:")
+        print("step3")
 if __name__ == "__main__":
     yoyo()
     Yoyo().yoyoketang()
