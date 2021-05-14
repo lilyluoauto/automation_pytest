3d361f0fc1b2077d22bcff26c7a4b394e6c90d5b
lilyauto <licy_luo@163.com>
2021-05-15 07:45:48 +0800

    add into master

---
+++ util/add_to_master.py
@@ -0,0 +1,8 @@
+# coding:utf-8
+# !/usr/bin/python
+# ========================================================
+# Project: automationTest
+# Creator: lilyluo
+# Create time: 2021-05-15 07:45
+# IDE: PyCharm
+# =========================================================
