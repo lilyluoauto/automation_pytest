c7256975fda52c264ca3c7fcf7460bc2ed7aa2d5
lilyauto <licy_luo@163.com>
2021-05-15 08:41:22 +0800

    step4:add file into test

---
+++ util/add_to_test.py
@@ -0,0 +1,9 @@
+# coding:utf-8
+# !/usr/bin/python
+# ========================================================
+# Project: automationTest
+# Creator: lilyluo
+# Create time: 2021-05-15 08:40
+# IDE: PyCharm
+# =========================================================
+#step1: add file into test
